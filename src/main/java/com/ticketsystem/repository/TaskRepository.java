7e7f162926cfaae2dea9afd9c4987b62c456b4c6
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 17:37:15 +0100

    creata query per creare task

--- src/main/java/com/ticketsystem/repository/TaskRepository.java
+++ src/main/java/com/ticketsystem/repository/TaskRepository.java
@@ -1,9 +1,20 @@
 package com.ticketsystem.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ticketsystem.entity.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Long>{
+import jakarta.transaction.Transactional;
 
+public interface TaskRepository extends JpaRepository<Task, Long>{
+	
+	@Modifying
+	@Transactional
+	@Query(value = "INSERT INTO task (deadline,descrizione,status,id_project) VALUES (:deadLine,:description,:status,:projectId)",nativeQuery = true)
+	void save(@Param("deadLine") LocalDateTime deadLine, @Param("description") String description , @Param("status") String status, @Param("projectId") Long id );
 }

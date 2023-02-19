9dae2ca4f38a810166672704aa6fc4cb22684b09
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 17:27:09 +0100

    creato endpoint per creare tasks

--- src/main/java/com/ticketsystem/controller/ProjectController.java
+++ src/main/java/com/ticketsystem/controller/ProjectController.java
@@ -5,11 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ticketsystem.entity.Project;
+import com.ticketsystem.entity.Task;
 import com.ticketsystem.service.ProjectService;
 
 @RestController
@@ -23,6 +25,15 @@ public class ProjectController {
 	public ResponseEntity<Project> assignProject(@PathVariable Long projectId, @RequestParam Long pmId) {
 		try {
 			projectService.assignProjectToPm(projectId, pmId);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PostMapping("/tasks")
+	public ResponseEntity<Task> createTaskForProject(@PathVariable Long projectId, @RequestBody Task task) {
+		try {
 			return new ResponseEntity<>(HttpStatus.CREATED);
 		}catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);

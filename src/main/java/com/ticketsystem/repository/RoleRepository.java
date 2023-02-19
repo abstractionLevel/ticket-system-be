7aac2726f5262a4f156e2c39f5b74fae85889aac
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 23:15:37 +0100

    create query per recuperare tutti i ruole ed implementata nel servzio

--- src/main/java/com/ticketsystem/repository/RoleRepository.java
+++ src/main/java/com/ticketsystem/repository/RoleRepository.java
@@ -1,9 +1,15 @@
 package com.ticketsystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ticketsystem.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
+
+	@Query(value="SELECT * FROM role")
+	List<Role> getAllRole();
 
 }
--- src/main/java/com/ticketsystem/service/impl/RoleServiceImpl.java
+++ src/main/java/com/ticketsystem/service/impl/RoleServiceImpl.java
@@ -2,18 +2,22 @@ package com.ticketsystem.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ticketsystem.entity.Role;
+import com.ticketsystem.repository.RoleRepository;
 import com.ticketsystem.service.RoleService;
 
 @Service
 public class RoleServiceImpl implements RoleService{
 
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	@Override
 	public List<Role> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		return roleRepository.getAllRole();
 	}
 
 }

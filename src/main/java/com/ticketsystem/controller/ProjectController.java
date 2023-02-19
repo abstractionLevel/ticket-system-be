14cf7f49535a014703ff96ddc0b0bd41550008e4
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 19:13:35 +0100

    aggiunto ll controller per i tasks

--- src/main/java/com/ticketsystem/controller/ProjectController.java
+++ src/main/java/com/ticketsystem/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package com.ticketsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -48,6 +51,16 @@ public class ProjectController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);		
 		}
 	
+	}
+	
+	@GetMapping("{projectId}/tasks")
+	public ResponseEntity<List<Task>> getTasks() {
+		try {
+		
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);		
+		}
 	}
 
 }

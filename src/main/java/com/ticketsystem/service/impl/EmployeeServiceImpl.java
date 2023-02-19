39bbed061d6f413ac839d3b952f9b0e6da3a76af
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 16:35:38 +0100

    aggiunte le annotation @service

--- src/main/java/com/ticketsystem/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/ticketsystem/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ticketsystem.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ticketsystem.entity.Employee;
 import com.ticketsystem.repository.EmployeeRepository;
 import com.ticketsystem.service.EmployeeService;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
 	@Autowired
--- src/main/java/com/ticketsystem/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/ticketsystem/service/impl/ProjectServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ticketsystem.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ticketsystem.entity.Employee;
 import com.ticketsystem.service.EmployeeService;
 import com.ticketsystem.service.ProjectService;
 
+
+@Service
 public class ProjectServiceImpl  implements ProjectService {
 	
 	@Autowired

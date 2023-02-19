09d09d6976bd72fc912f52a8e398378a66351bcc
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 17:13:32 +0100

    imp annotation

--- src/main/java/com/ticketsystem/repository/ProjectRepository.java
+++ src/main/java/com/ticketsystem/repository/ProjectRepository.java
@@ -1,13 +1,18 @@
 package com.ticketsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ticketsystem.entity.Project;
 
+import jakarta.transaction.Transactional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long>{
 	
+	@Modifying
+	@Transactional
 	@Query(value="UPDATE project  p SET p.id_pm :idPm WHERE p.id = :projectId", nativeQuery = true)
 	void assignToPm(@Param("projectId") Long projectId , @Param("idPm") Long idPm);
 

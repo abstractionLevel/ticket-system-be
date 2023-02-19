2f52f22efd1f74a10259463db62646d513067425
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 22:11:20 +0100

    aggiunte annotation

--- src/main/java/com/ticketsystem/repository/TaskAssignmentRepository.java
+++ src/main/java/com/ticketsystem/repository/TaskAssignmentRepository.java
@@ -1,13 +1,18 @@
 package com.ticketsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ticketsystem.entity.TaskAssignment;
 
+import jakarta.transaction.Transactional;
+
 public interface TaskAssignmentRepository extends JpaRepository<TaskAssignment, Long>{
 	
+	@Modifying
+	@Transactional
 	@Query(value = "INSERT INTO task_assignment (developer_id, task_id) VALUES (:devId,:taskId)", nativeQuery = true)
 	void save(@Param("taskId") Long taskId, @Param("devId") Long devId);
 

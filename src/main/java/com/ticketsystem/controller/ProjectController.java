af5ae7f06ca1d5b57ec061b6765cc5eb72193584
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 16:19:42 +0100

    endpoint per assegnare il progetto al pm

--- src/main/java/com/ticketsystem/controller/ProjectController.java
+++ src/main/java/com/ticketsystem/controller/ProjectController.java
@@ -1,12 +1,26 @@
 package com.ticketsystem.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ticketsystem.entity.Project;
 
 @RestController
 @RequestMapping("api/project")
 public class ProjectController {
 	
-	
+	@PostMapping("/pm/{pmId}/assign-project")
+	public ResponseEntity<Project> assignProject(@PathVariable Long pmId, @RequestParam Long projectId) {
+		try {
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 }

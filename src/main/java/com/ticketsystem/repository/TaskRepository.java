2c0c9840420ebddfc30441a1e94eb748f0c2a06a
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 17:31:55 +0100

    import task

--- src/main/java/com/ticketsystem/repository/TaskRepository.java
+++ src/main/java/com/ticketsystem/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.ticketsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ticketsystem.entity.Task;
+
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
 }

fa1afddc5c1decf6a37f7cd727804249630aa1b5
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 16:29:54 +0100

    creata query per prelevare l'impiegato tramite id

--- src/main/java/com/ticketsystem/repository/EmployeeRepository.java
+++ src/main/java/com/ticketsystem/repository/EmployeeRepository.java
@@ -1,11 +1,14 @@
 package com.ticketsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ticketsystem.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 	
-	
+	@Query(value="SELECT * FROM employee WHERE id = :id ", nativeQuery=true )
+	Employee getById(@Param("id") Long id);
 
 }
--- src/main/java/com/ticketsystem/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/ticketsystem/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,20 @@
 package com.ticketsystem.service.impl;
 
-public class EmployeeServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ticketsystem.entity.Employee;
+import com.ticketsystem.repository.EmployeeRepository;
+import com.ticketsystem.service.EmployeeService;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Override
+	public Employee findById(Long id) {
+		return  employeeRepository.getById(id);
+
+	}
 
 }

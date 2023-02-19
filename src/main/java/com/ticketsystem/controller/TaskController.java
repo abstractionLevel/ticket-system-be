1830d7e82b64b661426e5c8ff738440de8d23182
Ugo De Lucia <deluciaugo@gmail.com>
2023-02-19 21:40:04 +0100

    implementato controller per assegnare il task al dev

--- src/main/java/com/ticketsystem/controller/TaskController.java
+++ src/main/java/com/ticketsystem/controller/TaskController.java
@@ -1,11 +1,24 @@
 package com.ticketsystem.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
 
+	@PostMapping("/{id}/assigned-developer")
+	public ResponseEntity<?> assignTaskToDeveloper(@PathVariable Long id, @RequestParam Long devId ) {
+		try {
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 }
